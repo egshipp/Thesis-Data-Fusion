0739cfa99e1847bab73789509b0fd301b8ac54b8
Elena Shipp <egshipp@ncsu.edu>
2025-09-27 19:20:09 -0400

    Loglike function updated

--- NHPP2_Update.R
+++ NHPP2_Update.R
@@ -67,15 +67,15 @@ par(mfrow = c(1,1))
 # Source 1  --------------------------------------------------------------------
 
 set.seed(111)
-sigma_1 <- 0.01
+sigma_2 <- 0.01
 nrow <- length(nhpp_discretize$yrow)
 ncol <- length(nhpp_discretize$xcol)
 
-mu_1 <- -0.5 * sigma_1^2
-f_1 <- rnorm(nrow * ncol, mean = mu_1, sd = sigma_1)
-exp_f_1 <- exp(f_1)
+mu_1 <- -0.5 * sigma_2^2
+f <- rnorm(nrow * ncol, mean = mu_1, sd = sigma_2)
+exp_f <- exp(f)
 
-lambda_1 <- nhpp_discretize * exp_f_1
+lambda_1 <- nhpp_discretize * exp_f
 
 nhpp_1 <- rpoispp(lambda_1)
 
@@ -91,13 +91,13 @@ par(mfrow=(c(1,1)))
 
 # Source 2 ----------------------------------------------------------------------
 
-sigma_2 <- 1
+tau_2 <- 1
 alpha <- 0.75
 
-f_2 <- rnorm(nrow * ncol, mean = alpha, sd = sigma_2)
-exp_f_2 <- exp(f_2)
+g <- rnorm(nrow * ncol, mean = alpha, sd = tau_2)
+exp_g <- exp(g)
 
-lambda_2 <- nhpp_discretize * exp_f_2
+lambda_2 <- nhpp_discretize * exp_g
 
 nhpp_2 <- rpoispp(lambda_2)
 
@@ -116,3 +116,74 @@ plot(nhpp_sim)
 plot(nhpp_1)
 plot(nhpp_2)
 par(mfrow=(c(1,1)))
+
+# Data frame creation ----------------------------------------------------------
+
+## X grid
+X_grid <- as.data.frame(coords)
+colnames(X_grid) <- c("x", "y")
+X_grid$covariate <- covariate
+
+## Source 1 (small var)
+X_1 <- as.data.frame(nhpp_1)
+nn_X_1 <- nncross(nhpp_1, cov_field_ppp)
+X_1$covariate <- cov_field_ppp$marks[nn_X_1$which]
+
+## Source 2 (large var)
+X_2 <- as.data.frame(nhpp_2)
+nn_X_2 <- nncross(nhpp_2, cov_field_ppp)
+X_2$covariate <- cov_field_ppp$marks[nn_X_2$which]
+
+# MCMC --------------------------------------------------------------------------
+
+
+loglike <- function(parameters, data) {
+  
+  log_lambda_points1 <- parameters$beta[1] + parameters$beta[2] * data$X_1$covariate + parameters$f[data$nn_index_1]
+  term1 <- sum(log_lambda_points1)
+  
+  log_lambda_grid1 <- parameters$beta[1] + parameters$beta[2] * data$X_grid$covariate + parameters$f
+  lambda_grid1 <- exp(log_lambda_grid1)
+  term2 <- sum(lambda_grid1 * data$cell_area)
+  
+  log_lambda_points2 <- parameters$beta[1] + parameters$beta[2] * data$X_2$covariate + parameters$g[data$nn_index_2]
+  term3 <- sum(log_lambda_points2)
+  
+  log_lambda_grid2 <- parameters$beta[1] + parameters$beta[2] * data$X_grid$covariate + parameters$g
+  lambda_grid2 <- exp(log_lambda_grid2)
+  term4 <- sum(lambda_grid2 * data$cell_area)
+  
+  likelihood <- (term1 - term2) + (term3 - term4)
+  return(likelihood)
+}
+
+
+
+
+# Attempting --------------------------------------------------------------------
+
+
+data <- list(X_grid = X_grid, 
+             X_1 = X_1, 
+             X_2 = X_2,
+             cell_area  = (diff(win$xrange) / grid_res) * (diff(win$yrange) / grid_res),
+             nn_index_1 = nn_X_1$which,
+             nn_index_2 = nn_X_2$which)
+
+parameters <- list(beta = c(0,0),
+                   f = f,
+                   g = g,
+                   sigma_2 = 0.2,
+                   tau_2 = 1) 
+
+priors <- list(beta_mean = c(0,0), 
+               beta_sd = c(10,10), 
+               beta_prop_sd = c(0.1, 0.1),
+               f_mean = 0, 
+               a_0 = 0.5,
+               b_0 = 0.5)
+
+iters <- 7500
+
+sim <- driver(parameters, priors, data, iters)
+
