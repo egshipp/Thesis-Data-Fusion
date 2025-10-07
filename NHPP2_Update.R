6afb1cd93b6d2bc41f95e0d48743b4c3b5481512
Elena Shipp <egshipp@ncsu.edu>
2025-10-07 12:20:03 -0400

    IT WORKSSSSS

--- NHPP2_Update.R
+++ NHPP2_Update.R
@@ -165,16 +165,14 @@ loglike <- function(parameters, data) {
   lambda_grid1 <- exp(log_lambda_grid1)
   term2 <- sum(lambda_grid1 * data$cell_area)
   
-  # REMOVING G for testing #
-  # log_lambda_points2 <- parameters$beta[1] + parameters$beta[2] * data$X_2$covariate + parameters$g[data$nn_index_2]
-  # term3 <- sum(log_lambda_points2)
-  # 
-  # log_lambda_grid2 <- parameters$beta[1] + parameters$beta[2] * data$X_grid$covariate + parameters$g
-  # lambda_grid2 <- exp(log_lambda_grid2)
-  # term4 <- sum(lambda_grid2 * data$cell_area)
-  
-  likelihood <- (term1 - term2) 
-  # + (term3 - term4)
+  log_lambda_points2 <- parameters$beta[1] + parameters$beta[2] * data$X_2$covariate + parameters$g[data$nn_index_2]
+  term3 <- sum(log_lambda_points2)
+
+  log_lambda_grid2 <- parameters$beta[1] + parameters$beta[2] * data$X_grid$covariate + parameters$g
+  lambda_grid2 <- exp(log_lambda_grid2)
+  term4 <- sum(lambda_grid2 * data$cell_area)
+
+  likelihood <- (term1 - term2) + (term3 - term4)
   return(likelihood)
 }
 
@@ -183,10 +181,8 @@ update_betas<- function(parameters, priors, data){
   
   beta_cand <- rnorm(length(parameters$beta), mean = parameters$beta, sd = priors$beta_prop_sd) 
   
-  params_top <- list(beta = beta_cand, f = parameters$f)
-  #, g = parameters$g) # G REMOVED
-  params_bottom <- list(beta = parameters$beta, f = parameters$f)
-  #, g = parameters$g) # G REMOVED
+  params_top <- list(beta = beta_cand, f = parameters$f, g = parameters$g)
+  params_bottom <- list(beta = parameters$beta, f = parameters$f, g = parameters$g)
   
   # Posteriors
   post_top <- loglike(params_top, data) + sum(dnorm(beta_cand, mean = priors$beta_mean, sd = priors$beta_sd, log = TRUE))
@@ -242,46 +238,46 @@ update_f <- function(parameters, priors, data){
   }
 }
 
-# G REMOVED
-## Updating g - Source 2 measurement error
-# update_g <- function(parameters, priors, data){
-#   
-#   # Choosing ellipse (nu) from prior (g)
-#   nu <- as.vector(MASS::mvrnorm(n = 1, mu = rep(0, length(parameters$g)), Sigma = diag(parameters$tau_2, length(parameters$g))))
-#   
-#   # Log likelihood threshold (finding log(y))
-#   
-#   u <- runif(1, min = 0, max = 1)
-#   
-#   log_y <- loglike(parameters, data) + log(u)
-#   
-#   # Draw an initial proposal for theta
-#   
-#   theta <- runif(1, min = 0, max = 2*pi)
-#   theta_min <- theta - 2*pi
-#   theta_max <- theta
-#   
-#   repeat {
-#     # Calculate g'
-#     g_prime <- as.vector(parameters$g*cos(theta) + nu*sin(theta))
-#     
-#     params_prime <- parameters
-#     params_prime$g <- g_prime
-#     
-#     # Shrinking bracket
-#     if(loglike(params_prime, data) > log_y){
-#       parameters$g <- g_prime
-#       return(parameters) 
-#     } else {
-#       if(theta < 0) {
-#         theta_min <- theta
-#       } else {
-#         theta_max <- theta
-#       }
-#       theta <- runif(1, theta_min, theta_max)
-#     }
-#   }
-# }
+
+# Updating g - Source 2 measurement error
+update_g <- function(parameters, priors, data){
+
+  # Choosing ellipse (nu) from prior (g)
+  nu <- as.vector(MASS::mvrnorm(n = 1, mu = rep(0, length(parameters$g)), Sigma = diag(parameters$tau_2, length(parameters$g))))
+
+  # Log likelihood threshold (finding log(y))
+
+  u <- runif(1, min = 0, max = 1)
+
+  log_y <- loglike(parameters, data) + log(u)
+
+  # Draw an initial proposal for theta
+
+  theta <- runif(1, min = 0, max = 2*pi)
+  theta_min <- theta - 2*pi
+  theta_max <- theta
+
+  repeat {
+    # Calculate g'
+    g_prime <- as.vector(parameters$g*cos(theta) + nu*sin(theta))
+
+    params_prime <- parameters
+    params_prime$g <- g_prime
+
+    # Shrinking bracket
+    if(loglike(params_prime, data) > log_y){
+      parameters$g <- g_prime
+      return(parameters)
+    } else {
+      if(theta < 0) {
+        theta_min <- theta
+      } else {
+        theta_max <- theta
+      }
+      theta <- runif(1, theta_min, theta_max)
+    }
+  }
+}
 
 ## Updating source 1 variance using Gibbs Sampling- sigma_2
 
@@ -298,35 +294,34 @@ update_sigma_2 <- function(parameters, priors, data){
   
 }
 
-# g REMOVED
-## Updating source 2 variance using Gibbs Sampling - tau_2
-# 
-# update_tau_2 <- function(parameters, priors, data){
-#   n <- length(parameters$g)
-#   
-#   alpha_post <- priors$a_0_tau + n/2
-#   beta_post  <- priors$b_0_tau  + 0.5 * sum((parameters$g - parameters$alpha)^2)
-#   
-#   # Draw samples from Inverse-Gamma
-#   parameters$tau_2 <- 1 / rgamma(1, shape = alpha_post, rate = beta_post)
-#   
-#   return(parameters)
-#   
-# }
-# 
-# ## Updating source 2 mean using Gibbs Sampling - alpha
-#   
-# update_alpha <- function(parameters, priors, data){
-#   n <- length(parameters$g)
-#   
-#   denom <- (n / parameters$tau_2) + (1 / priors$phi)
-#   mu_post <- ( sum(parameters$g) / parameters$tau_2 / priors$phi ) / denom
-#   sigma_post <- sqrt(1 / denom)
-#   
-#   parameters$alpha <- rnorm(1, mean = mu_post, sd = sigma_post)
-#   
-#   return(parameters)
-# }
+# Updating source 2 variance using Gibbs Sampling - tau_2
+
+update_tau_2 <- function(parameters, priors, data){
+  n <- length(parameters$g)
+
+  alpha_post <- priors$a_0_tau + n/2
+  beta_post  <- priors$b_0_tau  + 0.5 * sum((parameters$g - parameters$alpha)^2)
+
+  # Draw samples from Inverse-Gamma
+  parameters$tau_2 <- 1 / rgamma(1, shape = alpha_post, rate = beta_post)
+
+  return(parameters)
+
+}
+
+## Updating source 2 mean using Gibbs Sampling - alpha
+
+update_alpha <- function(parameters, priors, data){
+  n <- length(parameters$g)
+
+  denom <- (n / parameters$tau_2) + (1 / priors$phi)
+  mu_post <- ( sum(parameters$g) / parameters$tau_2 / priors$phi ) / denom
+  sigma_post <- sqrt(1 / denom)
+
+  parameters$alpha <- rnorm(1, mean = mu_post, sd = sigma_post)
+
+  return(parameters)
+}
 
 ## Wrapper function
 
@@ -339,11 +334,10 @@ driver <- function(parameters, priors, data, iters){
   #Posterior containers
   out$beta=matrix(NA,nrow = length(parameters$beta),ncol = iters)
   out$f=matrix(NA, nrow = length(parameters$f), ncol = iters)
-  # out$g=matrix(NA, nrow = length(parameters$g), ncol = iters) # g REMOVED
+  out$g=matrix(NA, nrow = length(parameters$g), ncol = iters)
   out$sigma_2=matrix(NA, nrow = 1, ncol = iters)
-  # out$tau_2=matrix(NA, nrow = 1, ncol = iters) # g REMOVED
-  # out$alpha=matrix(NA, nrow = 1, ncol = iters) # g REMOVED
-  
+  out$tau_2=matrix(NA, nrow = 1, ncol = iters)
+  out$alpha=matrix(NA, nrow = 1, ncol = iters)
   
   for(k in 1:iters){
     parameters <- update_betas(parameters, priors, data) 
@@ -351,18 +345,18 @@ driver <- function(parameters, priors, data, iters){
 
     parameters <- update_f(parameters, priors, data) 
     out$f[,k] <- parameters$f
-# 
-#     parameters <- update_g(parameters, priors, data) # g REMOVED
-#     out$g[,k] <- parameters$g
+
+    parameters <- update_g(parameters, priors, data) # g REMOVED
+    out$g[,k] <- parameters$g
 
     parameters <- update_sigma_2(parameters, priors, data) 
     out$sigma_2[,k] <- parameters$sigma_2
-# 
-#     parameters <- update_alpha(parameters, priors, data) # g REMOVED
-#     out$alpha[,k] <- parameters$alpha
-# 
-#     parameters <- update_tau_2(parameters, priors, data) # g REMOVED
-#     out$tau_2[,k] <- parameters$tau_2
+
+    parameters <- update_alpha(parameters, priors, data) # g REMOVED
+    out$alpha[,k] <- parameters$alpha
+
+    parameters <- update_tau_2(parameters, priors, data) # g REMOVED
+    out$tau_2[,k] <- parameters$tau_2
   }
   return(out)
 }
@@ -372,29 +366,27 @@ driver <- function(parameters, priors, data, iters){
 
 data <- list(X_grid = X_grid, 
              X_1 = X_1, 
-           #  X_2 = X_2,
+            X_2 = X_2,
              cell_area  = (diff(win$xrange) / grid_res) * (diff(win$yrange) / grid_res),
-             nn_index_1 = nn_X_1$which
-            # nn_index_2 = nn_X_2$which
-            )
+             nn_index_1 = nn_X_1$which,
+            nn_index_2 = nn_X_2$which)
 
 parameters <- list(beta = c(0,0),
                    f = f,
-                   # g = g,
-                   sigma_2 = 0.1
-                   # alpha = 0.75,
-                   # tau_2 = 0.5
-)
+                   g = g,
+                   sigma_2 = 0.1,
+                   alpha = 0.75,
+                   tau_2 = 0.5)
 
 priors <- list(beta_mean = c(0,0), 
                beta_sd = c(10,10), 
                beta_prop_sd = c(0.075, 0.075),
                f_mean = 0,
                a_0_sigma = 0.5,
-               b_0_sigma = 0.5
-               # a_0_tau = 2,
-               # b_0_tau = 1,
-               # phi = 10
+               b_0_sigma = 0.5,
+               a_0_tau = 2,
+               b_0_tau = 1,
+               phi = 10
                )
 
 iters <- 5000
@@ -404,29 +396,29 @@ burnin <- 0
 sim <- driver(parameters, priors, data, iters)
 
 beta_post <- sim$beta[, (burnin+1):iters]
-#alpha_post <- sim$alpha[,(burnin+1):iters]
+alpha_post <- sim$alpha[,(burnin+1):iters]
 sigma_2_post <- sim$sigma_2[,(burnin+1):iters]
-#tau_2_post <- sim$tau_2[,(burnin+1):iters]
+tau_2_post <- sim$tau_2[,(burnin+1):iters]
 f_post <- sim$f[,(burnin+1):iters]
-#g_post <- sim$g[,(burnin+1):iters]
+g_post <- sim$g[,(burnin+1):iters]
 
 apply(beta_post, 1, mean)
 apply(beta_post, 1, sd)
 
 mean(sigma_2_post)
 sd(sigma_2_post)
-# 
-# mean(tau_2_post)
-# sd(tau_2_post)
-# 
-# mean(alpha_post)
-# sd(alpha_post)
+
+mean(tau_2_post)
+sd(tau_2_post)
+
+mean(alpha_post)
+sd(alpha_post)
 
 mean(f_post)
 sd(f_post)
-#    
-# mean(g_post)
-# sd(g_post)
+
+mean(g_post)
+sd(g_post)
 
 
 # Posterior Plots ----------------------------------------------------------------
@@ -476,10 +468,10 @@ par(mfrow = c(1,1))
 
 plot(sim$beta[1,], type = "l", main = "Beta Trace Plot")
 plot(sim$f[1,], type = "l", main = "f trace plot")
-# plot(sim$g[1,], type = "l", main = "g trace plot")
+plot(sim$g[1,], type = "l", main = "g trace plot")
 plot(sim$sigma_2[1,], type = "l", main = "sigma_2 trace plot")
-# plot(sim$tau_2[1,], type = "l", main = "tau_2 trace plot")
-# plot(sim$alpha[1,], type = "l", main = "alpha trace plot")
+plot(sim$tau_2[1,], type = "l", main = "tau_2 trace plot")
+plot(sim$alpha[1,], type = "l", main = "alpha trace plot")
 
 # Testing -------------------------------------------------------------------------
 loglike(parameters, data)
